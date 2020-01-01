60340b4e4cbd9828611faf407c60ebb0f6ca2008
Nadezda Stepanova <hope_air@mail.ru>
2020-01-02 01:45:58 +0400

    for my first homework

---
+++ lesson_3.py
@@ -0,0 +1,24 @@
+#______СтепановаНС________Урок 3-Списки.Словари.Кортежи.Множества__________
+
+file = open('C:\Users\Надежда\Documents\ПРОГРАММИРОВАНИЕ_статьи и материалы\Университет_ИИ\Курс_Python-разработчик\myfirstone\WaP.txt')
+
+with open(file='WaP.txt', mode='r', encoding='utf-8') as file:
+    for line in WaP:
+        for word in line.replace(char, "") #методами строк очистить текст от знаков препинания;
+        for word in line.split('\n') #сформировать list со словами (split);
+file = str(word)
+        word = word.lower()
+        print(file.lower()) #привести все слова к нижнему регистру (map); \\ ЧЕРЕЗ "map" - ??? 0_о
+
+dict = {WaP}
+word = []
+WaP = {[word]'cout': +1} #получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
+
+
+file.sort(.......)
+    for i in range(5)
+
+file_set = set(word)
+print(type(file_set)
+
+#вывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set).
