cf2d77aff4918086b053cdfa6714ade00d4c4b68
Nadezda Stepanova <hope_air@mail.ru>
2020-01-07 18:18:44 +0400

    for my first homework

---
+++ divisor_master.py
@@ -0,0 +1,38 @@
+
+# 1) проверка числа на простоту (простые числа - это те числа у которых делители единица и они сами);
+if __name__ == '__lesson_5':
+
+def is_prime(n):
+    while n > 1:
+        for n % i == 0:
+        # n // n == 0
+            i += 1
+    return n
+print(is_prime(n))
+
+# 2) выводит список всех делителей числа;
+
+def all_dividers(n):
+    result = []
+    while i * i <= n:
+        if n % i == 0:
+            n //= i
+            result.append(i)
+        else:
+            i += 1
+    if  n > 1:
+        result.append(n)
+    return result
+print(all_dividers(n))
+
+# 3) выводит самый большой простой делитель числа.
+
+def greatest_pr_div(n):
+    mam = []
+    for n % i == 0:
+        if max < max_temp[i]:
+            max = max_temp[i]
+print(max)
+
+# КАК ЧЕРЕЗ ЛЯМБДУ:: greatest_pr_div() = lambda n: greatest_pr_div(i) > greatest_pr_div(i+1) if n > 1 else &
+# if ... lambda n, i: i if i>(i+1) else i+1
---
+++ lesson_5.py
@@ -0,0 +1,18 @@
+#______СтепановаНС________Урок 5-МОДУЛИ__________
+
+#Необходимо реализовать модуль divisor_master. Все функции модуля принимают на вход натуральные числа от 1 до 1000. Модуль содержит функции:
+
+import divisor_master
+
+for n in range (1, 1001):
+
+print(is_prime(n))
+print(all_dividers(n))
+print(greatest_pr_div(n))
+
+# print(divisor_master.is_prime(n))
+# print(divisor_master.all_dividers(n))
+# print(divisor_master.greatest_pr_div(n))
+
+
+
