e0e7798ea15b03dad13ad570dfd19c1bc8855267
Nadezda Stepanova <hope_air@mail.ru>
2020-01-26 16:40:59 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	divisor_master.py

---
+++ test_lesson_6.py
@@ -0,0 +1,61 @@
+# --------ТЕСТИРОВАНИЕ ФУНКЦИЙ--------
+
+# проверка на простоту
+def is_prime(n):
+    i = 2
+    while n > i:
+        if n % i == 0:
+            break
+        i += 1
+    return i == n
+print(is_prime(n))
+
+def test_is_prime:
+    assert is_prime(7)  == True
+
+def test_is_prime:
+    assert is_prime(344) == False
+
+# выводит список всех делителей числа;
+def all_dividers(n):
+    result = []
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            n //= i
+            result.append(i)
+        else:
+            i += 1
+    if  n > 1:
+        result.append(n)
+    return result
+print(all_dividers(23244))
+
+def test_all_dividers:
+    assert all_dividers(7) == [7]
+
+def test_all_dividers:
+    try:
+        if n < 0:
+    except LoZero as ex:
+        print('отрицательное число')
+    else:
+        i += 1
+    finally:
+        return result
+
+# выводит самый большой простой делитель числа.
+def greatest_pr_div(n):
+    prime_num = all_dividers(n)
+    max_num = 0
+    for i in prime_num:
+        if i > max_num:
+            max_num = i
+    return(max_num)
+print(greatest_pr_div(23244))
+
+def test_greatest_pr_div:
+    assert greatest_pr_div(149) == [149]
+
+def test_greatest_pr_div:
+    assert greatest_pr_div(-149) == [0]
